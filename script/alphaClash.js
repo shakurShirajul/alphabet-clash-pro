1f0f68f94a567167ef5d389597cb5900d9830069
shakurShirajul <siirajulisllam@gmail.com>
2024-02-14 14:31:42 +0600

    setBackgroundColor Added

--- script/alphaClash.js
+++ script/alphaClash.js
@@ -1,4 +1,18 @@
+function continueGame(){
+    // step -1: generate a random alphabet
+    //  let alphabet = String.fromCharCode(Math.floor((Math.random() * 26) + 97));
+    
+    //step-2 : hide the start screen and show the game screen
+    const alphabet = getARandomAlphabet();
+    document.getElementById('currentAlphabet').innerText = alphabet;
+    // document.querySelector(".startScreen").style.display="none";
+    // document.querySelector(".gameScreen").style.display="flex";
+    
+    setBackgroundColorById(alphabet);
+}
+
 function playNow(){
     hideElementById('home');
     showElementById('playground')
+    continueGame();
 }
--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,20 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId){
+    const  element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+    console.log(elementId);
+}
+
+function getARandomAlphabet(){
+    // get or create an alphabet array
+    const  alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split(' ');
+
+    // get a random index between 0 - 25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    return  alphabetString[index];
+}
